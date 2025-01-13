8a9ee60e28a9a664af4f3ed793e06d4e90bfeb63
Prabh <156637376+Not-Prabhpreet@users.noreply.github.com>
2025-01-13 08:10:01 -0330

    Update FraudDetectionForm.jsx

--- frontend/src/components/FraudDetectionForm.jsx
+++ frontend/src/components/FraudDetectionForm.jsx
@@ -42,7 +42,7 @@ const FraudDetectionForm = () => {
 
       console.log('Sending request data:', requestData);
 
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/predict`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
